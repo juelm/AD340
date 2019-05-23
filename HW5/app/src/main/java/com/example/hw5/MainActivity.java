fb5e532ce826ffa46cad56f2e25554bf26a77a5a
Matt Juel <mattcjuel@gmail.com>
2019-05-22 19:02:09 -0700

    Updated Espresso Config

--- HW5/app/src/main/java/com/example/hw5/MainActivity.java
+++ HW5/app/src/main/java/com/example/hw5/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.SearchView;
 
 
 public class MainActivity extends AppCompatActivity {
